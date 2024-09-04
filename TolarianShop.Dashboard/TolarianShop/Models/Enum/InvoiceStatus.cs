644c95b10459c3ea059f5ffbb2e5ff67721dcbad
Mateus Mourao <mateusvmourao@gmail.com>
2024-09-04 20:46:18 -0300

    feat: criação de models relativos a nota fiscal

---
+++ TolarianShop.Dashboard/TolarianShop/Models/Enum/InvoiceStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace TolarianShop.Models.Enum;
+
+public enum InvoiceStatus
+{
+    Issued = 1,
+    ChargeMade = 2,
+    PaymentOverdue = 3,
+    PaymentMade = 4,
+}
---
+++ TolarianShop.Dashboard/TolarianShop/Models/InvoiceFilter.cs
@@ -0,0 +1,10 @@
+﻿using TolarianShop.Models.Enum;
+
+namespace TolarianShop.Models;
+public class InvoiceFilter
+{
+    public int? IssueMonth { get; set; }
+    public int? ChargeMonth { get; set; }
+    public int? PayMonth { get; set; }
+    public InvoiceStatus? Status { get; set; }
+}
