d84015078793a3741e0947a2192f87581ee6b2b5
Mateus Mourao <mateusvmourao@gmail.com>
2024-09-08 01:42:11 -0300

    feat: data e migrations

---
+++ TolarianShop.Dashboard/TolarianShop/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TolarianShop.Models;
+
+namespace TolarianShop.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+    public DbSet<NotaFiscal> NotaFiscal { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<NotaFiscal>()
+            .Property(nf => nf.Valor)
+            .HasColumnType("decimal(18,2)");
+    }
+}
---
+++ TolarianShop.Dashboard/TolarianShop/Migrations/20240906040056_InitialCreate.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TolarianShop.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "NotaFiscal",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NomePagador = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NumeroNotaFiscal = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DataEmissao = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DataCobrança = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DataPagamento = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Valor = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    DocumentoNotaFiscal = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DocumentoPagamento = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Status = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NotaFiscal", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NotaFiscal");
+        }
+    }
+}
